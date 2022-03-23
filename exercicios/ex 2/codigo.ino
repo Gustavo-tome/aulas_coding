3988ca8324a01133217eae6395572d3b011cbf2b
Gustavo-tome <79268186+Gustavo-tome@users.noreply.github.com>
2022-03-23 08:22:05 -0300

    Create codigo.ino

---
+++ exercicios/ex 2/codigo.ino
@@ -0,0 +1,20 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(10, OUTPUT);
+   pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(10, HIGH);
+  delay(1000);
+  digitalWrite(10, LOW);
+  delay(000); 
+  
+  digitalWrite(4, HIGH);
+  delay(1000);
+  digitalWrite(4, LOW);
+  delay(000); 
+}
